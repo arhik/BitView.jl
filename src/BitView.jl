d22eb0c2c541c35faa1babd17ce782e41af020a8
arhik <arhik23@gmail.com>
2023-01-18 10:52:13 +0530

    renaming main struct

--- src/BitView.jl
+++ src/BitView.jl
@@ -2,11 +2,13 @@ module BitView
 
 # Write your package code here.
 
+export bitview
 
-mutable struct BitView{T, N} <: AbstractArray{T, N}
+
+mutable struct BitViewArray{T, N} <: AbstractArray{T, N}
     chunks::SubArray
     dims::NTuple{N,Int}
-    function BitView{T, N}(a::Array{T, N}) where {T, N}
+    function BitViewArray{T, N}(a::Array{T, N}) where {T, N}
     	nbytes = sizeof(T).*size(a)
     	dims = 8 .* nbytes
     	nc = div.(nbytes, sizeof(UInt64))
@@ -16,18 +18,18 @@ mutable struct BitView{T, N} <: AbstractArray{T, N}
     end
 end
 
-bitview(a::Array{T, N}) where {T, N} = BitView{T, N}(a)
+bitview(a::Array{T, N}) where {T, N} = BitViewArray{T, N}(a)
 
-Base.size(b::BitView{T, N}) where {T, N} = b.dims
-Base.size(b::BitView{T, N}, a::Int) where {T, N} = getindex(b.dims, a)
-Base.length(b::BitView{T, N}) where {T, N} = sizeof(UInt64)*( b.dims |> prod )
+Base.size(b::BitViewArray{T, N}) where {T, N} = b.dims
+Base.size(b::BitViewArray{T, N}, a::Int) where {T, N} = getindex(b.dims, a)
+Base.length(b::BitViewArray{T, N}) where {T, N} = sizeof(UInt64)*( b.dims |> prod )
 
-function Base.getindex(b::BitView{T, N}, a::Int) where {T, N}
+function Base.getindex(b::BitViewArray{T, N}, a::Int) where {T, N}
 	(idx, subidx) = div(a - 1, 8*sizeof(T), RoundDown) + 1, mod(a, 8*sizeof(T)) - 1
 	return (b.chunks[idx] & (UInt64(1) << subidx)) != 0
 end
 
-function Base.setindex!(b::BitView{T, N}, c::Bool, a::Int) where {T, N}
+function Base.setindex!(b::BitViewArray{T, N}, c::Bool, a::Int) where {T, N}
 	(idx, subidx) = div(a - 1, 8*sizeof(T), RoundDown) + 1, mod(a - 1, 8*sizeof(T)) - 1
 	b.chunks[idx] = (b.chunks[idx] & (UInt64(1) << subidx)) != 0
 end
