34b4739dccbba720ba65ed42a9d34e43c50c87dc
arhik <arhik23@gmail.com>
2023-01-21 23:53:20 +0530

    test cases

--- src/BitView.jl
+++ src/BitView.jl
@@ -1,14 +1,22 @@
 module BitView
 
 # Write your package code here.
+# 
 
-export bitview
-
+export bitview, BitViewArray
 
 mutable struct BitViewArray{T, N} <: AbstractArray{T, N}
-    chunks::SubArray
+    subArray::SubArray
     dims::NTuple{N,Int}
     function BitViewArray{T, N}(a::Array{T, N}) where {T, N}
+    	nbytes = sizeof(T).*size(a)
+    	dims = 8 .* nbytes
+    	nc = div.(nbytes, sizeof(UInt64))
+    	a = reinterpret(UInt64, a)
+        b = new(view(a, :), dims)
+        return b
+    end
+    function BitViewArray{T, N}(a::Base.ReinterpretArray{T, N}) where {T, N}
     	nbytes = sizeof(T).*size(a)
     	dims = 8 .* nbytes
     	nc = div.(nbytes, sizeof(UInt64))
@@ -19,20 +27,62 @@ mutable struct BitViewArray{T, N} <: AbstractArray{T, N}
 end
 
 bitview(a::Array{T, N}) where {T, N} = BitViewArray{T, N}(a)
+bitview(a::Base.ReinterpretArray{T, N}) where {T, N} = BitViewArray{T, N}(a)
 
 Base.size(b::BitViewArray{T, N}) where {T, N} = b.dims
 Base.size(b::BitViewArray{T, N}, a::Int) where {T, N} = getindex(b.dims, a)
 Base.length(b::BitViewArray{T, N}) where {T, N} = sizeof(UInt64)*( b.dims |> prod )
 
-function Base.getindex(b::BitViewArray{T, N}, a::Int) where {T, N}
-	(idx, subidx) = div(a - 1, 8*sizeof(T), RoundDown) + 1, mod(a, 8*sizeof(T)) - 1
-	return (b.chunks[idx] & (UInt64(1) << subidx)) != 0
+function Base.getindex(b::BitViewArray{T, N}, I...) where {T, N}
+	(idx, subidx) = div.(I .- 1, 8 .* sizeof(T), RoundDown) .+ 1, mod.(I, 8 .* sizeof(T)) .- 1
+	return (b.subArray[idx...] & (ones(UInt64, length(subidx)) .<< subidx)) != 0
 end
 
+# function Base.getindex(b::BitViewArray{T, N}, I::Union{Colon, AbstractRange}) where {T, N}
+	# Base._getindex(IndexStyle(b.subArray), b, I)
+# end
+# 
 function Base.setindex!(b::BitViewArray{T, N}, c::Bool, a::Int) where {T, N}
 	(idx, subidx) = div(a - 1, 8*sizeof(T), RoundDown) + 1, mod(a - 1, 8*sizeof(T)) - 1
-	b.chunks[idx] = (b.chunks[idx] & (UInt64(1) << subidx)) != 0
+	b.subArray[idx] = (b.subArray[idx] & (UInt64(1) << subidx)) != 0
 end
 
+# function Base.setindex!(b::BitViewArray{T, N},)
 
+IndexStyle(::Type{<:BitViewArray}) = IndexLinear()
+IndexStyle(::BitViewArray) = IndexLinear()
+
+# Base.isassigned(::BitViewArray) =
+
+#function iterate
+
+# similar 
+
+# reshape
+
+# copyto
+
+# array 
+
+# push!
+
+# append!
+
+# 
+
+# Dummy Array
+
+struct DummyArray{T} <: AbstractArray{T, 1}
+	a::Array{T, 1}
 end
+
+function Base.getindex(a::DummyArray, I...)
+	Base._getindex(IndexStyle(a.a), a.a, Base.to_indices(a.a, I)...)
+end
+
+Base.size(d::DummyArray) = Base.size(d.a)
+
+a = DummyArray(rand(3))
+
+
+
---
+++ src/testarrays.jl
@@ -0,0 +1,19 @@
+using BitView
+
+# TEST 1D Array
+a = rand(32)
+b = bitview(a)
+
+# TEST 2D Array
+a = rand(4, 4)
+b = bitview(a)
+
+# TEST 3D Array
+a = rand(4, 4, 4)
+b = bitview(a)
+
+# TEST ND Array
+a = rand(32)
+b = bitview(a)
+
+
