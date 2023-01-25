5a4554c2c2a62cae8990380f3df4de13285cf5bb
arhik <arhik23@gmail.com>
2023-01-26 00:00:09 +0530

    Another dimension

--- src/BitView.jl
+++ src/BitView.jl
@@ -15,19 +15,17 @@ Base.uinttype(::Type{Gray{N0f8}}) = UInt8
 
 mutable struct BitViewArray{T, N} <: AbstractArray{T, N}
     subArray::SubArray
-    dims::NTuple{N,Int}
     function BitViewArray{T, N}(a::Array{T, N}) where {T, N}
-    	dims = (sizeof(T)*8, ones(Int, ndims(a)-1)...) .* size(a)
+    	dims = (size(a)..., sizeof(T)*8)
     	a = reinterpret(Base.uinttype(T), a)
     	ranges = size(a) .|> Base.Fix1(UnitRange, 1)
-        b = new(view(a, ranges...), dims)
+        b = new(view(a, ranges...))
         return b
     end
     function BitViewArray{T, N}(a::Base.ReinterpretArray{T, N}) where {T, N}
-    	dims = (sizeof(T)*8, ones(Int, ndims(a)-1)...) .* size(a)
     	a = reinterpret(Base.uinttype(T), a)
     	ranges = size(a) .|> Base.Fix1(UnitRange, 1)
-        b = new(view(a, ranges...), dims)
+        b = new(view(a, ranges...))
         return b
     end
 end
@@ -35,19 +33,23 @@ end
 bitview(a::Array{T, N}) where {T, N} = BitViewArray{T, N}(a)
 bitview(a::Base.ReinterpretArray{T, N}) where {T, N} = BitViewArray{T, N}(a)
 
-Base.size(b::BitViewArray{T, N}) where {T, N} = b.dims
-Base.size(b::BitViewArray{T, N}, a::Int) where {T, N} = getindex(b.dims, a)
-Base.length(b::BitViewArray{T, N}) where {T, N} = sizeof(eltype(b))*( b.dims |> prod )
+Base.size(b::BitViewArray{T, N}) where {T, N} = (b.subArray |> size)..., sizeof(T)*8)
+Base.size(b::BitViewArray{T, N}, a::Int) where {T, N} = getindex(size(b), a)
+Base.length(b::BitViewArray{T, N}) where {T, N} = ( size(b) |> prod )
+
+# function Base.getindex(b::BitViewArray{T, N}, I...) where {T, N}
+	# stride = (sizeof(eltype(b))*8, ones(Int, ndims(b) - 1)...)
+	# (idx, subidx) = div.(I .- 1, stride, RoundDown) .+ 1, mod(first(I), stride |> first) - 1
+	# return (b.subArray[idx...] & ((Base.uinttype(eltype(b)) |> one) << subidx)) != 0
+# end
 
 function Base.getindex(b::BitViewArray{T, N}, I...) where {T, N}
-	stride = (sizeof(eltype(b))*8, ones(Int, ndims(b)-1)...)
-	(idx, subidx) = div.(I .- 1, stride, RoundDown) .+ 1, mod(first(I), stride |> first) - 1
+	(idx, subidx) = I[1:end-1], last(I)
 	return (b.subArray[idx...] & ((Base.uinttype(eltype(b)) |> one) << subidx)) != 0
 end
 
 function Base.getindex(b::BitViewArray{T, N}, I::CartesianIndex) where {T, N}
-	stride = (sizeof(eltype(b))*8, ones(Int, ndims(b)-1)...)
-	(idx, subidx) = div.(I.I .- 1, stride, RoundDown) .+ 1, mod(first(I.I), stride |> first) - 1
+	(idx, subidx) = I.I[1:end-1], last(I.I)
 	return (b.subArray[idx...] & ((Base.uinttype(eltype(b)) |> one) << subidx)) != 0
 end
 
@@ -56,8 +58,7 @@ function Base.getindex(b::BitViewArray{T, N}, I::Union{Colon, AbstractRange}) wh
 end
 
 function Base.setindex!(b::BitViewArray{T, N}, c::Bool, I...) where {T, N}
-	stride = (sizeof(eltype(b))*8, ones(Int, ndims(b)-1)...)
-	(idx, subidx) = div.(I .- 1, stride, RoundDown) .+ 1, mod(first(I), stride |> first) - 1
+	(idx, subidx) = I[1:end-1], last(I)
 	b.subArray[idx...] = ifelse(
 		c,
 		(b.subArray[idx...] | ((Base.uinttype(eltype(b)) |> one) << subidx)),
@@ -66,8 +67,7 @@ function Base.setindex!(b::BitViewArray{T, N}, c::Bool, I...) where {T, N}
 end
 
 function Base.setindex!(b::BitViewArray{T, N}, c::Bool, I::CartesianIndex) where {T, N}
-	stride = (sizeof(eltype(b))*8, ones(Int, ndims(b)-1)...)
-	(idx, subidx) = div.(I.I .- 1, stride, RoundDown) .+ 1, mod(first(I.I), stride |> first) - 1
+	(idx, subidx) = I.I[1:end-1], last(I.I)
 	b.subArray[idx...] = ifelse(
 		c,
 		(b.subArray[idx...] | ((Base.uinttype(eltype(b)) |> one) << subidx)),
